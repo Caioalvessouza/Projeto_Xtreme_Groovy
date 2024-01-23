9066888296360ae2b102ee16216c13a023fa73fc
Caio Rodrigues <caiorodrigues143@hotmail.com>
2024-01-23 02:04:56 -0300

    extratcion

--- notebooks/src/extraction.py
+++ notebooks/src/extraction.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import os
+
 def load_data():
-    csv_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'bikes_completed.csv')
+    # Obtemos o caminho do diretório atual do script
+    current_dir = os.path.dirname(__file__)
+    
+    # Construímos o caminho completo para o arquivo CSV
+    csv_path = os.path.abspath(os.path.join(current_dir, '..', 'data', 'bikes_completed.csv'))
+    
+    # Lemos o arquivo CSV
     return pd.read_csv(csv_path)
 
