0e5af3b72aaa9a5b1d97e309312b3d1d8bb534f3
Caio Rodrigues <caiorodrigues143@hotmail.com>
2024-01-23 01:59:08 -0300

    extratction

--- notebooks/src/extraction.py
+++ notebooks/src/extraction.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
-
+import os
 def load_data():
-    return pd.read_csv("data/bikes_completed.csv")
+    csv_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'bikes_completed.csv')
+    return pd.read_csv(csv_path)
+
