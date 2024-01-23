801684954e9402cc65b1f3b49039b28a0432641d
Caio Rodrigues <caiorodrigues143@hotmail.com>
2024-01-23 02:14:05 -0300

    ajuste 6

--- notebooks/src/extraction.py
+++ notebooks/src/extraction.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def load_data():
-    csv_path = "C:\\Users\\Caio\\Documents\\cientista de dados\\phyton\\Git para cientista de dados\\projeto\\notebooks\\data\\bikes_completed.csv"
+    csv_path = "C:\\Users\\Caio\\Documents\\cientista de dados\\phyton\\Git para cientista de dados\\projeto\\data\\bikes_completed.csv"
     return pd.read_csv(csv_path)
