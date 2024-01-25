2a367ee38655a5801dcfeef254078cdec9138092
Caio Rodrigues <caiorodrigues143@hotmail.com>
2024-01-25 00:08:03 -0300

    delete de arquivos copiados

--- extraction.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def load_data():
-    csv_path = "C:\\Users\\Caio\\Documents\\cientista de dados\\phyton\\Git para cientista de dados\\projeto\\data\\bikes_completed.csv"
-    return pd.read_csv(csv_path)
